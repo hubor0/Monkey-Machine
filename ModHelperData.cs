428d2b14fe5c1217e32ca691425092b834917a3b
hubor <minecraftcostam2009@gmail.com>
2026-08-02 18:34:00 +0200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,11 +2,11 @@ namespace MonkeyMachine;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "45.0";
-    public const string Version = "2.0.0";
+    public const string WorksOnVersion = "55.0";
+    public const string Version = "2.0.1";
     public const string Name = "Monkey Machine Tower";
 
-    public const string Description = "Adds a new custom Monkey Machine tower. Complete with all 15 tiers and a Paragon coming soon!";
+    public const string Description = "Adds a new custom Monkey Machine tower. Complete with all 15 tiers, and a Paragon and 555 version available now on Tewtiy's membership server!";
 
     public const string RepoOwner = "hubor0"; // TODO add your github username hero, also in the download url in README.md
     public const string RepoName = "Monkey-Machine"; // TODO add your repo name here, also in the download url in README.md
